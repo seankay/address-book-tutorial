3f98d4356025c6a9735c48d17cfffae5fd64ceeb
seankay <f.sean.kay@gmail.com>
2014-11-23 16:52:25 -0500

    Add persistence!

--- src/address_book/core/routes/address_book_routes.clj
+++ src/address_book/core/routes/address_book_routes.clj
@@ -3,30 +3,21 @@
             [ring.util.response :as response]
             [address-book.core.views.address-book-layout :refer [common-layout
                                                                  add-contact-form
-                                                                 read-contact]]))
-
-(def contacts (atom [{:id 1 :name "Sean Kay" :phone "(555) 555-5555" :email "sean@example.com"}
-                     {:id 2 :name "Jarrod Taylor" :phone "(555) 666-7777" :email "Jarrod@JarrodCTaylor.com"}
-                     {:id 3 :name "James Dalton"  :phone "(123) 567-8901" :email "Cooler@Roadhouse.com"}
-                     {:id 4 :name "Johnny Utah"   :phone "(543) 333-1234" :email "J.Utah@Buckeyes.com"}]))
-
-(defn next-id []
-  (->>
-    @contacts
-    (map :id)
-    (apply max)
-    (+ 1)))
+                                                                 read-contact]]
+            [address-book.core.models.database :refer [db]]
+            [address-book.core.models.query-defs :as query]))
 
 (defn post-route [request]
   (let [name (get-in request [:params :name])
         phone (get-in request [:params :phone])
         email (get-in request [:params :email])]
-    (swap! contacts conj {:id (next-id) :name name :phone phone :email email})
+    (query/insert-contact<! { :name name :phone phone :email email } {:connection db})
     (response/redirect "/")))
 
 (defn get-route [request]
   (common-layout
-    (for [contact @contacts] (read-contact contact))
+    (for [contact (query/all-contacts {} {:connection db})]
+      (read-contact contact))
     (add-contact-form)))
 
 (defn example-get [request]
@@ -36,4 +27,3 @@
 (defroutes address-book-routes
   (GET "/" [] get-route)
   (POST "/post" [] post-route))
-
--- test/address_book/core/address_book_tests.clj
+++ test/address_book/core/address_book_tests.clj
@@ -3,16 +3,38 @@
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
             [address-book.core.handler :refer :all]
-            [address-book.core.routes.address-book-routes :refer [contacts]]))
+            [address-book.core.models.database :refer [database db]]
+            [address-book.core.models.query-defs :as query]))
+
+(def test-db (database :test))
 
 (facts "Example GET and POST tests"
+  (with-state-changes [(before :facts (query/create-contacts-table-if-not-exists!
+                                        {} {:connection test-db})
+                               :after (query/drop-contacts-table!
+                                        {} {:connection test-db}))]
   (fact "Test GET"
-    (let [response (app (mock/request :get "/"))]
-      (:status response) => 200
-      (:body response) => (contains "<div class=\"column-1\">Sean Kay</div>")))
+    (with-redefs [db test-db]
+      (query/insert-contact<! {:name "Sean Kay"
+                                :phone "(321) 222-2222"
+                                :email "test@example.com"}
+                            {:connection test-db})
+      (query/insert-contact<! {:name "Test"
+                                :phone "(555) 222-2222"
+                                :email "test1@example.com"}
+                            {:connection test-db})
+      (let [response (app (mock/request :get "/"))]
+        (:status response) => 200
+        (:body response) => (contains "<div class=\"column-1\">Sean Kay</div>"))))
 
   (fact "Test POST"
-    (let [response (app (mock/request :post "/post" {:name "John Smith" :phone "(555) 253-3333" :email "test@example.com"}))
-          new-contact (filter #(= 5 (:id %)) @contacts)]
-      (:status response) => 302
-      (:name (first new-contact)) => "John Smith")))
+    (with-redefs [db test-db]
+      (count (query/all-contacts {} {:connection test-db})) => 0
+
+      (let [response (app (mock/request :post
+                                        "/post"
+                                        {:name "John Smith"
+                                         :phone "(555) 253-3333"
+                                         :email "test@example.com"}))]
+        (:status response) => 302
+        (count (query/all-contacts {} {:connection test-db})) => 1)))))
