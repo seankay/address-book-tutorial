677b96f9fd2a3f0faa940204106b01fbb1acf17f
seankay <f.sean.kay@gmail.com>
2014-11-23 12:55:19 -0500

    Test handler GET and POST routes

--- test/address_book/core/handler_test.clj
+++ test/address_book/core/handler_test.clj
@@ -1,14 +1,16 @@
 (ns address-book.core.handler-test
+  (:use midje.sweet)
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
             [address-book.core.handler :refer :all]))
 
-(deftest test-app
-  (testing "main route"
+(facts "Example GET and POST tests"
+  (fact "Test GET"
     (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-  
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+      (:status response) => 200
+      (:body response) => "Example GET"))
+
+  (fact "Test POST"
+    (let [response (app (mock/request :post "/post" {:example-post "Some data"}))]
+      (:status response) => 200
+      (:body response) => "You posted: Some data")))
